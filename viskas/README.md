bebcf31f83f0fe341d57c6cee29d2e3bb6b2ef85
Rengarmacia <mataspss@gmail.com>
2017-01-07 16:07:39 +0800

    :100:
    asd

--- viskas/README.md
+++
@@ -1,1 +0,0 @@
-# rengarmacia.github.io
--- viskas/senas psl/script.js
+++
@@ -1,6 +0,0 @@
-function mine(){
-var width1 = document.getElementById("myPage").offsetWidth;
-
-document.getElementById("width-meter").innerHTML = width1;
-
-}
