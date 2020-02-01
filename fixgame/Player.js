bf5802cca637e3c3b7d876131cf079f9a6852160
Matas <mataspss@gmail.com>
2020-02-01 12:10:58 +0000

    test 11

--- fixgame/Player.js
+++ fixgame/Player.js
@@ -37,7 +37,7 @@ class Player {
     if(this.timer < 30)
     {
       strokeWeight(this.timer/5);
-      line(this.x + this.size/2, (25 - this.timer)*15 + 100, this.x + this.size/2, this.y - this.timer*25);
+      line(this.x + this.size/2,  this.y - this.timer*25 + 100, this.x + this.size/2, this.y - this.timer*25);
       this.laserY = this.y - this.timer*25;
       strokeWeight(1);
       this.timer++;
